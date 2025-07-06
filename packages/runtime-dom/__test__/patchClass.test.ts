ed2a86fa51f2187952e2724030b20d435b5bb861
Yu Lia <1938793150@qq.com>
2025-07-06 13:34:45 +0800

    feat(test): patchClass

--- packages/runtime-dom/__test__/patchClass.test.ts
+++ packages/runtime-dom/__test__/patchClass.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { patchProp } from '../src/patchProp'
 
 describe('runtime-dom/patchClass', () => {
