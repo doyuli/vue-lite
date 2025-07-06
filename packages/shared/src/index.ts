e4a92441890e9cf484192c2ee77844057deff145
Yu Lia <1938793150@qq.com>
2025-07-06 18:44:36 +0800

    feat(shared): utils

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,23 +1,2 @@
-export function isObject(value: unknown): value is Record<string, unknown> {
-  return typeof value === 'object' && value !== null
-}
-
-export function isFunction(value: unknown): value is Function {
-  return typeof value === 'function'
-}
-
-export function isArray(value: unknown): value is Array<unknown> {
-  return Array.isArray(value)
-}
-
-export function isString(value: unknown): value is string {
-  return typeof value === 'string'
-}
-
-export function isOn(value: string): boolean {
-  return /^on[A-Z]/.test(value)
-}
-
-export function hasChanged(newValue: unknown, oldValue: unknown): boolean {
-  return !Object.is(newValue, oldValue)
-}
+export * from './shapeFlags'
+export * from './utils'
