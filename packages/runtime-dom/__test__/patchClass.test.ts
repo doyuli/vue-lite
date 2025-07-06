1322e058d35b83c8b4a64b7f1afa0e060342e20b
Yu Lia <1938793150@qq.com>
2025-07-06 12:51:09 +0800

    feat(runtime-dom): patchClass

---
+++ packages/runtime-dom/__test__/patchClass.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it, vi } from 'vitest'
+import { patchProp } from '../src/patchProp'
+
+describe('runtime-dom/patchClass', () => {
+  it('should be patch', () => {
+    const el = document.createElement('div')
+    patchProp(el, 'class', null, 'foo')
+    expect(el.className).toBe('foo')
+
+    patchProp(el, 'class', null, null)
+    expect(el.className).toBe('')
+  })
+})
